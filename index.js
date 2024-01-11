3260ff83facc685d56162089419e9244916dae90
TechBoxNorth <techboxnorth@gmail.com>
2024-01-11 09:51:56 +0100

    testing alert, prompt and confirm

---
+++ index.js
@@ -0,0 +1,12 @@
+alert("Click me to continue!");
+console.log("clicked");
+
+let usrname = prompt("State your designation");
+alert(`Welcome to the collective ${usrname}.`);
+
+let answer = confirm("Are you awake?");
+if (answer) {
+  console.log("Good morning!");
+} else {
+  console.log("Rise and shine!");
+}
